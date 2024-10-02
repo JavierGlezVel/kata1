77117a50a62b74b3458a4da6776ca749633fee44
Javier González Velázquez <javiergonzavelazquez@gmail.com>
2024-10-02 12:49:14 +0100

    Second Version

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(new Person("Juan", LocalDate.of(2005, 4, 4)));
     }
 }
